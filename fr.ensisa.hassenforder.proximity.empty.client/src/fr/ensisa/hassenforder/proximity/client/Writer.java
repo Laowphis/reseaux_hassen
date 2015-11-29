f0914c35a1eed127516512943a14d216ecb81798
Laowphis <john.fernandes89@gmail.com>
2015-11-29 14:14:32 +0100

    mise en place

---
+++ fr.ensisa.hassenforder.proximity.empty.client/src/fr/ensisa/hassenforder/proximity/client/Writer.java
@@ -0,0 +1,18 @@
+package fr.ensisa.hassenforder.proximity.client;
+
+import java.io.OutputStream;
+
+import fr.ensisa.hassenforder.network.BasicAbstractWriter;
+import fr.ensisa.hassenforder.network.Protocol;
+import fr.ensisa.hassenforder.proximity.model.Mode;
+
+public class Writer extends BasicAbstractWriter {
+
+	public Writer(OutputStream outputStream) {
+		super (outputStream);
+	}
+
+	public void connect(){
+		this.writeInt(1);
+	}
+}
